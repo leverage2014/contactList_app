cd0ae02f64aef88af63482ccbe0fe96d719de755
leverage2014 <myletter428@gmail.com>
2016-07-05 18:17:22 -0400

    update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,51 +3,52 @@ var contactListApp = angular.module('contactListApp', []);
 contactListApp.controller('listCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.greeting = 'Contact List App';
 	$scope.addFlag = false;
-
+	$scope.contact = {};
+	
 	$scope.getData = function(){
         $http.get('/contacts').success(function (response) {
             console.log("i got the data from server: \n");
             console.log(response);
             $scope.contactList = response;
+            $scope.contact = {};
         });
 	}
 
 	$scope.getData();
 
     $scope.addContact = function(){
-    	var newContact = $scope.contact;
-    	$scope.contactList.push($scope.contact);
-    	console.log('enter addContact');
-    	console.log($scope.contact);
-    	$scope.contact = null;
+
+    	if(typeof $scope.contact.name === 'undefined' ||
+    	   typeof $scope.contact.email === 'undefined' ||
+    	   typeof $scope.contact.number === 'undefined'){
+    		alert('Illegal input');
+    		return;
+    	}
+
+    	$http.post('/contacts', $scope.contact).success(function (response) {
+            console.log("resposne:" +response);
+            $scope.getData();
+        });
     };
 
     $scope.updateContact = function(){
-    
-    	if(typeof $scope.contact === 'undefined'){
-    	    throw new Error("Pease select a contact to edit!");
-    	}
-    	$scope.contactList[$scope.contact.index] = $scope.contact;
-    	$scope.contact = null;
-    	$scope.addFlag = false;
+        $http.put('/contacts/'+$scope.contact._id, $scope.contact).success(function (response) {
+            $scope.getData();
+        });
+        $scope.addFlag = false;
     };
 
-    $scope.remove = function(name){
-    	var foundIndex;
-    	$scope.contactList.forEach(function(contact,index){
-    		if(contact.name === name){
-    			foundIndex = foundIndex;
-    		}
-    	});
-
-    	$scope.contactList.splice(foundIndex, 1);
+    $scope.remove = function(id){
+    	console.log(id);
+    	$http.delete('/contacts/'+id).success(function (response) {
+            $scope.getData();
+        });
     };
 
     $scope.edit = function(one){
-    	$scope.contact = one;
-    	var index = $scope.contactList.indexOf(one);
-    	console.log(index);
-    	$scope.contact.index = index;
+
+    	// cut off the connection between selected and input 
+    	angular.copy(one, $scope.contact);
     	$scope.addFlag = true;
     };
 
